cc9234f77d794ab2788d1e1caee5587a6693c100
dewililiana <dewi.liliana@gmail.com>
2019-07-03 10:23:17 +0700

    pertama

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+/* program untuk menampilkan
+tulisan hello world ke layar
+dalam bahasa c++ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout<<"Hello world! "<<endl;  //perintah untuk menampilkan
+  cout<<"Welcome to c++"<<endl;
+  system ("pause");
+  return 0;
+}
